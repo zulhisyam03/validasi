f82d2252708652c915e32775329896e510ce4acd
zulhisyam03 <zul0342@gmail.com>
2021-07-08 19:04:52 +0800

    Selesai Tampilan Login Form

--- login-form/dist/index.php
+++ login-form/dist/index.php
@@ -7,6 +7,9 @@
 <link rel="stylesheet" href="./style.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
 <style type="text/css">
+  h1{
+    margin-top: -10px;
+  }
   h3{
     color: white;
     text-align: center;
@@ -14,39 +17,50 @@
   a{
     color: skyblue;
   }
+  img{
+    width: 300px;
+    height: 200px;
+  }
 </style>
 </head>
 <body>
 <!-- partial:index.partial.html -->
-<div class="login">
+
   <?php
     if(isset($_GET['registrasi']))
     {
       ?>
-        <h1>Daftar</h1>
-        <form method="post" name="regis">
-          <input type="text" name="u" placeholder="Username" required="required" />
-            <input type="password" name="p" placeholder="Password" required="required" />
-            <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
-        </form>
-        <p></p>
-        <h3>Sudah Punya Akun? <a href="../dist/">Login</a></h3>
+        <div class="login" style="top: 200px;">
+          <center><img src="../untad.png" alt=""></center>
+          <h1>Daftar</h1>
+          <form method="post" name="regis" action="regist.php">
+            <input type="text" name="nama_reg" placeholder="Nama Lengkap" required="required" />
+            <input type="text" name="nim_reg" placeholder="NIM" required="required">
+            <input type="text" name="email_reg" placeholder="E-Mail">
+            <input type="password" name="pass_reg" placeholder="Password" required="required" />
+            <button type="submit" class="btn btn-primary btn-block btn-large">Daftar</button>
+          </form>
+          <p></p>
+          <h3>Sudah Punya Akun? <a href="../dist/">Login</a></h3>
+        </div>
       <?php
     }
     else{
       ?>
-        <h1>Login</h1>
-        <form method="post" name=login>
-          <input type="text" name="u" placeholder="Username" required="required" />
-            <input type="password" name="p" placeholder="Password" required="required" />
-            <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
-        </form>
-        <p></p>
-        <h3>Belum Punya Akun? <a href="?registrasi">Daftar</a></h3>
+        <div class="login" style="top:200px;">
+          <center><img src="../untad.png" alt=""></center>
+          <h1>Login</h1>
+          <form method="post" name=login>
+            <input type="text" name="u" placeholder="Username" required="required" />
+              <input type="password" name="p" placeholder="Password" required="required" />
+              <button type="submit" class="btn btn-primary btn-block btn-large">Let me in.</button>
+          </form>
+          <p></p>
+          <h3>Belum Punya Akun? <a href="?registrasi">Daftar</a></h3>
+        </div>
       <?php
     }
   ?>
-</div>
 <!-- partial -->
   <script  src="./script.js"></script>
 
