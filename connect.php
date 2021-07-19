006e015d519f0737c90db2ed1d927c3c6f3e69da
zulhisyam03 <zul0342@gmail.com>
2021-07-20 01:47:35 +0800

    Delete ftp_sync dan tambahkan SFTP

--- connect.php
+++ connect.php
@@ -1,8 +1,8 @@
 <?php
-  $host = "localhost";
-  $user = "root";
-  $pass = "";
-  $data = "validasi";
+  $host = "sql303.epizy.com";
+  $user = "epiz_29182215";
+  $pass = "Borutouzumaki03";
+  $data = "epiz_29182215_validasi";
   
   $db = mysqli_connect($host,$user,$pass,$data)or die("Database Error");
 ?>
